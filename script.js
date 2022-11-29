d5f50814a55d20f956d8c3b3db1982fb652e6974
julia <93724114+xjuliaag@users.noreply.github.com>
2022-11-29 10:06:40 -0300

    Add files via upload
    Upload dos arquivos html, css e javascript. As imagens constam em uma pasta chamada img.

---
+++ script.js
@@ -0,0 +1,26 @@
+const switchModal = () => {
+  const modal = document.querySelector('.modal')
+  const actualStyle = modal.style.display
+  if(actualStyle == 'block') {
+    modal.style.display = 'none'
+  }
+  else {
+    modal.style.display = 'block'
+  }
+}
+
+const btn = document.querySelector('.modalBtn')
+btn.addEventListener('click', switchModal)
+
+window.onclick = function(event) {
+    const modal = document.querySelector('.modal')
+  if (event.target == modal) {
+    switchModal()
+  }
+}
+
+
+
+
+
+
